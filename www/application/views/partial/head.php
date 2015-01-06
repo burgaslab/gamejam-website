9f2e7a17e27233c326d1fad8ca8ab64bf6cfe687
elleche <elichenkova@gmail.com>
2015-01-06 20:45:01 +0200

    responsive fix

--- www/application/views/partial/head.php
+++ www/application/views/partial/head.php
@@ -2,6 +2,6 @@
 <meta property="og:image" content="<?php echo $this->config->base_url() ?>resource/img/fb_preview.png" />
 <meta charset="utf-8">
 <meta name="description" content="" />
-<meta name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="<?= $this->path->base ?>resource/img/favicon.ico">
 <?php require_once("style.php"); ?>

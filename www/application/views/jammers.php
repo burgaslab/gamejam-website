1ea224de66a38a3a8cc6954ed69257909f6d90b3
elleche <elichenkova@gmail.com>
2015-01-23 23:15:23 +0200

    added jammer resources

--- www/application/views/jammers.php
+++ www/application/views/jammers.php
@@ -10,9 +10,20 @@
 		<div class="row demo-row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div class="inner-container trans-black">
-					<p class="indented">
-					Тук ще публикуваме текуща информация, касаеща Jammer-ите от Бургас.
-					</p>
+				<ul>
+					<li>
+						<a href="https://docs.google.com/document/d/1pz0Vrjttv8IE4NTF3RYxqNuKjGfkFnxYX2aNo0T5Bu4/edit?usp=sharing" 
+						target="_blank">Политика на поведение</a>
+					</li>
+					<li>
+						<a href="http://globalgamejam.org/jammer-resources" 
+						target="_blank">Ресурси от GGJ</a>
+					</li>
+					<li>
+						<a href="https://docs.google.com/presentation/d/1gzJpXquXfzQH6tlUDVzO38W5OCOEa7h5xKccIYKdmQw/edit#slide=id.p" 
+						target="_blank">Lifecycle of a 48 hour game</a>
+					</li>
+				</ul>
 				</div>
 			</div>
 		</div>

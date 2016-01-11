40552379f4c5c37b9d16844ca6ffe1197fb087ac
Jani <sleepwalker.bg@gmail.com>
2016-01-11 13:00:38 +0200

    Updating event start time.

--- www/application/2016/config/custom_config.php
+++ www/application/2016/config/custom_config.php
@@ -7,7 +7,7 @@ $config["title"] = "Burgas Game Jam :: 29-31 януари 2016";
 $config["age_groups"] = array("12-16г.", "17-18г.", "18г.+");
 $config["occupations"] = array("ученик", "студент", "работещ", "безработен");
 
-$config["event_start"] = "2016-01-29 13:00:00";
+$config["event_start"] = "2016-01-29 14:00:00";
 $config["location"] = array(42.494730, 27.482612);
 /*
  * Use this from other config files like so:

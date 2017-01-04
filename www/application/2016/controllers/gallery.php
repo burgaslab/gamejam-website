52c66f531609cc65b67caed15ae7845222c0f79d
Jani <sleepwalker.bg@gmail.com>
2017-01-04 21:40:21 +0200

    add gallery photos

--- www/application/2016/controllers/gallery.php
+++ www/application/2016/controllers/gallery.php
@@ -5,6 +5,56 @@ require("base.php");
 class Gallery extends Base {
 
 	public function index() {
+		clearstatcache();
+		
+		$config['image_library'] = 'gd2';
+		$config['create_thumb'] = TRUE;
+		$config['maintain_ratio'] = TRUE;
+		$config['thumb_marker'] = '';
+		$config['width'] = 300;
+		$config['height'] = 150;
+		$this->load->library('image_lib', $config);
+		
+		$source_dir = 'resource/image/gallery/';
+		$thumb_dir = 'resource/image/gallery/thumbs/';
+		$image_ext = array('png','jpg','jpeg','gif');
+		$photos = array();
+		
+		if ($dh = opendir($source_dir)) {
+			while (($file = readdir($dh)) !== false) {
+				$ext = pathinfo($file, PATHINFO_EXTENSION);
+				if ($ext && in_array($ext, $image_ext)) {
+					$photos[] = $file;
+				}
+			}
+			closedir($dh);
+		}
+
+		if (!is_dir(FCPATH . $thumb_dir)) {
+			mkdir(FCPATH . $thumb_dir, 0777);
+		}
+		$count = count($photos);
+		foreach ($photos as $file) {
+			$thumb = FCPATH . $thumb_dir . $file;
+	
+			if(!file_exists($thumb)) {
+				$config['source_image'] = $source_dir . $file;
+				$config['new_image'] = $thumb_dir . $file;
+					
+				$this->image_lib->initialize($config);
+				$this->image_lib->resize();
+				$this->image_lib->clear();
+			}
+		}
+		
+		$data = array(
+				'photos' => $photos,
+				'dir' => $source_dir,
+				'thumb_dir' => $thumb_dir,
+		);
+		
+		$this->view->set("gallery", $data);
+		
 		$this->render("gallery");
 	}
 }
--- www/application/2016/views/gallery.tpl
+++ www/application/2016/views/gallery.tpl
@@ -1,15 +1,16 @@
 {extends "base.tpl"}
 
 {block "content"}
-{*
 <div class="richtext">
 <h1>Галерия</h1>
+{*
 	<h2>Pellentesque quis elit non lectus gravida blandit</h2>
 	<p>Aliquam libero. Vivamus nisl nibh, iaculis vitae, viverra sit amet, ullamcorper vitae, turpis. </p>
 </div>
-<ul class="assets gallery">
-	<li><a href="temp.jpg"><img src="temp.jpg" alt="" /></a></li>
-	<li><a href="temp.jpg"><img src="temp.jpg" alt="" /></a></li>
-</ul>
 *}
+<ul class="assets gallery">
+	{foreach $gallery.photos as $i}
+	<li><a href="{$gallery.dir}{$i}"><img src="{$gallery.thumb_dir}{$i}" alt="" /></a></li>
+	{/foreach}
+</ul>
 {/block}
